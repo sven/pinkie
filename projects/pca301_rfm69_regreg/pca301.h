292bb12c5254566e2ecbde0483442c8520e2990a
Sven Bachmann <dev@mcbachmann.de>
2018-05-26 11:15:52 +0200

    projects/pca301_rfm69_regreg: enable auto-poll per default

--- projects/pca301_rfm69_regreg/pca301.h
+++ projects/pca301_rfm69_regreg/pca301.h
@@ -62,7 +62,7 @@
 #define PCA301_DFL_CHAN                     1   /* default channel */
 #define PCA301_DFL_TIMEOUT_RES_MS         500   /* default response timeout */
 #define PCA301_DFL_FLG_PAIR                 0   /* disable pairing */
-#define PCA301_DFL_FLG_POLL_AUTO            0   /* disable auto poll on switch detect */
+#define PCA301_DFL_FLG_POLL_AUTO            1   /* auto-poll on switch detect flag */
 #define PCA301_DFL_RETRIES                  0   /* no retry on timeout */
 #define PCA301_DFL_FLG_FRAME_DUMP           0   /* disable frame dump */
 

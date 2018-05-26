39bfbc936b72f2fbd457b24a04839cdaef28b935
Sven Bachmann <dev@mcbachmann.de>
2018-05-26 11:17:03 +0200

    projects/pca301_rfm69_regreg: improved description of pairing and frame dump flag

--- projects/pca301_rfm69_regreg/pca301.h
+++ projects/pca301_rfm69_regreg/pca301.h
@@ -61,10 +61,10 @@
 
 #define PCA301_DFL_CHAN                     1   /* default channel */
 #define PCA301_DFL_TIMEOUT_RES_MS         500   /* default response timeout */
-#define PCA301_DFL_FLG_PAIR                 0   /* disable pairing */
+#define PCA301_DFL_FLG_PAIR                 0   /* pairing enable flag */
 #define PCA301_DFL_FLG_POLL_AUTO            1   /* auto-poll on switch detect flag */
 #define PCA301_DFL_RETRIES                  3   /* resend retries */
-#define PCA301_DFL_FLG_FRAME_DUMP           0   /* disable frame dump */
+#define PCA301_DFL_FLG_FRAME_DUMP           0   /* frame dump enable flag */
 
 
 /*****************************************************************************/
